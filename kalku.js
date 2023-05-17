c136b7a0c953b10f5147d914d594638e760b2e20
amanda_jeanette2005 <tirzamandajeanette@gmail.com>
2023-05-17 13:22:33 +0700

    revisi

--- kalku.js
+++ kalku.js
@@ -1,57 +1,69 @@
 const btn = document.querySelectorAll('.btn');
 const input = document.getElementById('input');
-let number1 = null
-let number2 = null 
-let operator = null 
+let number1 = null;
+let number2 = null;
+let operator = null;
 
 console.log(btn.length);
-for(i = 0; i < btn.length; i++){
-    btn[i].onclick = function(e){
-        const value = e.target.innerText
-   
+for(let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function(e) {
+        const value = e.target.innerText;
 
-    if(value == '1' || value == '2' || value == '3' || value == '4' || value == '5' || value == '6' || value == '7' || value == '8' || value == '9' || value == '0' ){
-      
-        if(number2 == null && operator !== null){
-            input.value = value 
-        }else {
-            input.value += value
+        if (value >= '0' && value <= '9') {
+            if (number2 == null && operator !== null) {
+                input.value = value;
+                number2 = parseFloat(input.value);
+            } else if (number2 == null) {
+                input.value += value;
+                number2 = parseFloat(input.value);
+            } else {
+                input.value += value;
+                number2 = parseFloat(input.value);
+            }
         }
 
-        if(operator == null){
-            number1 = input.value
-        }else if(operator !== null) {
-            number2 = input.value
+        if (value === '+' || value === '-' || value === '*' || value === '/') {
+            if (number1 == null) {
+                number1 = parseFloat(input.value);
+                input.value = '';
+            }
+            operator = value;
         }
-    }
-    if(value == '+' || value == '-' || value == '*' || value == '/'){
-        operator = value
-    }
 
-    if(value == 'C'){
-        input.value = ''
-    }
+        if (value === 'C') {
+            number1 = null;
+            number2 = null;
+            operator = null;
+            input.value = '';
+        }
 
-    if(value == '='){
-        console.log(number1, number2);
+        if (value === '=') {
+            console.log(number1, number2);
 
-        if(operator == "+"){
-            operator = parseInt(number1) + parseInt(number2)
-            input.value = operator
+            if (operator === '+') {
+                operator = number1 + number2;
+                input.value = operator;
+            }
+            if (operator === '-') {
+                operator = number1 - number2;
+                input.value = operator;
+            }
+            if (operator === '*') {
+                operator = number1 * number2;
+                input.value = operator;
+            }
+            if (operator === '/') {
+                if (number2 !== 0) {
+                    operator = number1 / number2;
+                    input.value = operator;
+                } else {
+                    input.value = 'Error: Division by zero';
+                }
+            }
+            
+            number1 = null;
+            number2 = null;
+            operator = null;
         }
-        if(operator == '-'){
-            operator = number1 - number2
-            input.value = operator
-        }
-        if(operator == '*'){
-            operator = number1 * number2
-            input.value = operator
-        }
-        if(operator == '/'){
-            input.value = operator
-        }
-    }
-
-}
-     
+    });
 }
